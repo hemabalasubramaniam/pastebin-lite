34ea29b1361acf11ded5567eaa51748d16e4a63a
Hema-Bala <hema@frontier.xyz>
2025-12-31 13:53:39 +0530

    feat: vercel config added

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 require("dotenv").config();
 const express = require("express");
+const PORT = process.env.PORT || 3000;
+const APP_URL = process.env.APP_URL || `http://localhost:${PORT}`;
+
 const pasteRoutes = require("./routes/paste.routes");
 
 const app = express();
 app.use(express.json());
+
 app.use("/api", pasteRoutes);
 
-if (!process.env.VERCEL) {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Pastebin API running at http://localhost:${PORT}`);
-  });
-}
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Pastebin API running at ${APP_URL}`);
+});
