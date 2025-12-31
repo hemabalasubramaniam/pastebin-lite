4f0f579b2a4f1e4d600eb7be6c7a7fb4731cd92f
Hema-Bala <hema@frontier.xyz>
2025-12-31 12:47:16 +0530

    fix: updated the main file

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
 const express = require("express");
-const PORT = process.env.PORT || 3000;
-const APP_URL = process.env.APP_URL || `http://localhost:${PORT}`;
-
 const pasteRoutes = require("./routes/paste.routes");
 
 const app = express();
 app.use(express.json());
-
 app.use("/api", pasteRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Pastebin API running at ${APP_URL}`);
-});
+if (!process.env.VERCEL) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Pastebin API running at http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
